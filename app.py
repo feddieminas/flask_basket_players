33965f39f7d661e9b888bf84cb372441a7118604
Faidon Dermesonoglou <faidon.dermesonoglou@gmail.com>
2019-02-09 19:16:32 +0000

    templates added - addplayer draft form - mongo add db config

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
+import json
 from flask import Flask, render_template, redirect, request, url_for, session
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 #app.secret_key = "fmd270584" 
 
+app.config["MONGO_DBNAME"] = 'dcd_basketball'
+app.config["MONGO_URI"] = 'mongodb://root:dcd_basketball1234@ds119049.mlab.com:19049/dcd_basketball'
+
+mongo = PyMongo(app) 
 
 @app.route('/')
 @app.route('/login')
@@ -41,6 +47,34 @@ def insert_login():
             #mongodb find records before redirecting to list summary and parameter the records
     
     return redirect(url_for('get_username'))
+
+
+@app.route('/list_summary')
+def get_list():
+    return render_template("listsummary.html")
+
+
+@app.route('/add_player')
+def add_player():
+    #userId=1 #we take it then from session variable, might make it global var
+    return render_template("addplayer.html")
+
+
+@app.route('/insert_player', methods=['POST'])
+def insert_player(): #need to pass UserId
+    mydictReq={'userId': 1, 'position': request.form["optPosition"], 'name': request.form["player_name"], 
+    'gender': request.form["optGender"], 'birth_region': request.form["optBirthRegion"], 
+    'discipline': {'disc1':[request.form["disc1"],request.form["disc1_rate"]], 
+    'disc2':[request.form["disc2"],request.form["disc2_rate"]],
+    'disc3':[request.form["disc3"],request.form["disc3_rate"]]},
+    'virtual_meet': {'times_see':3, 'go_for':'coffee'}}
+    print('mydictReq',mydictReq)
+    print('json', json.dumps(mydictReq, ensure_ascii=False))
+    
+    ubp = mongo.db.users_basket_players
+    ubp.insert_one(mydictReq)
+    
+    return redirect(url_for('add_player'))
 
 
 if __name__ == '__main__':
